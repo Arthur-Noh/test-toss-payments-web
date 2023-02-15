4430b57e850ca4dcb28dc09b86a539d7b8e5d2f8
Arthur-Noh <dkagod97@gmail.com>
2023-02-15 15:57:27 +0900

    feat(page): make default pages

---
+++ src/pages/default/index.tsx
@@ -0,0 +1,10 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const Default = () => {
+    return (
+        <div>default page</div>
+    );
+};
+
+export default Default;
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,10 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>main page</div>
+    );
+};
+
+export default Main;
